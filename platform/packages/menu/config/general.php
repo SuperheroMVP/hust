2d82b2111cd509974e78892dd88b6d3b79d852d9
vanhuy <vanhuy174>
2020-07-01 09:31:31 +0700

    hust 01/07

--- platform/packages/menu/config/general.php
+++ platform/packages/menu/config/general.php
@@ -5,7 +5,8 @@ return [
         'header-menu' => 'Header Navigation',
         'main-menu'   => 'Main Navigation',
         'footer-menu' => 'Footer Navigation',
-        'content-menu'=>'Content Menu'
+        'content-menu'=>'Content Menu',
+        'daotao-menu'=>'Menu Đào tạo',
     ],
     'cache'     => [
         'enabled' => env('CACHE_FRONTEND_MENU', false),
